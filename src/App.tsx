1449fd66f8e782329f9be74b0bf8e46c0417ca58
maazellahikhan19960 <maazellahikhan19960@gmail.com>
2020-07-08 16:17:08 +0500

    project created and button added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <button className="button-class">click here</button>
+    </div>
+  );
+}
+
+export default App;
